6fe9e875b6b6be90cdf3e974e70b2ec353bf3ffa
casebenton <casebenton@gmail.com>
2016-07-13 15:14:37 -0700

    migrate to shield utils

--- index.js
+++ index.js
@@ -1,3 +1,32 @@
-const {Recommend} = require('lib/Recommend.js');
+/* 1. the modules needed to turn this into a STUDY */
+const xutils = require('lib/shield-addon-utils');
+const variationsMod = require('lib/variations');
 
-Recommend.start();
+/* 2. configuration / setup constants for the study.
+ *  These are only ones needed, or supported
+ */
+const forSetup = {
+  name: 'Addon Recommendation Test', // unique for Telemetry
+  choices: Object.keys(variationsMod.variations), // names of branches.
+  duration: 7,   // in days,
+  surveyUrl: 'https://qsurvey.mozilla.com/s3/Shield-Study-Example-Survey',
+};
+
+// 3. Study Object (module singleton);
+const ourConfig = xutils.xsetup(forSetup);
+const thisStudy = new xutils.Study(ourConfig, variationsMod);
+
+/* 4. usual bootstrap / jetpack main function */
+function main(options, callback) { // eslint-disable-line no-unused-vars
+  xutils.generateTelemetryIdIfNeeded().then(() => {
+    xutils.handleStartup(options, thisStudy);
+  });
+}
+
+function onUnload(reason) {
+  xutils.handleOnUnload(reason, thisStudy);
+}
+
+/* 5. usual bootstrap addon exports */
+exports.main = main;
+exports.onUnload = onUnload;
